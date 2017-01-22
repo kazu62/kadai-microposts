eef1ebb909189bbd2c9aaba415364fa88acb44af
kazu62 <jelsaremsrod.ko@gmail.com>
2017-01-22 16:06:07 +0000

    timeline

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,5 +23,18 @@ abstract class Controller extends BaseController
         ];
     }
     
-    
+ public function index()
+    {
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+        }
+        return view('welcome', $data);
+    }
 }
